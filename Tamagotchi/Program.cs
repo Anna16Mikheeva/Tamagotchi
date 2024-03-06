affa55905385a4eb5465259bf4a1ad7e20950342
Анна <anna.mav31@gmail.com>
2024-03-06 13:15:02 +0700

    Создание проекта

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Tamagotchi.Pet;
+
+namespace Tamagotchi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("##############################################################\n" +
+                "##############################DOG#############################\n\n");
+            Console.WriteLine("Pet's name: ");
+            Pet pet = new Pet();
+            //pet. = Console.ReadLine();
+        }
+    }
+}
